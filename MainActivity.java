1d351b7922145e2cda72091370e214682876963d
Neel1999 <86246437+Neel1999@users.noreply.github.com>
2021-07-12 13:39:21 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -10,25 +10,35 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private EditText editText;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = findViewById(R.id.textView2);
-        EditText editText = findViewById(R.id.editText);
-        Button button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView2);
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "The Number has been Entered", Toast.LENGTH_LONG).show();
-                String str = editText.getText().toString();
-                int i = Integer.parseInt(str);
-                double p = 2.20462 * i;
-                textView.setText(String.valueOf(p));
-            }
-        });
-
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "The Number has been Entered", Toast.LENGTH_LONG).show();
+//                String str = editText.getText().toString();
+//                int i = Integer.parseInt(str);
+//                double p = 2.20462 * i;
+//                textView.setText(String.valueOf(p));
+//            }
+//        });
     }
+    public void calculate (View view){
+        String str = editText.getText().toString();
+        int i = Integer.parseInt(str);
+        double d = 2.20462 * i;
+        textView.setText("The required values in Pounds is "+ String.format("%.2f",d));
+        Toast.makeText(this, "Thank You For Using the App", Toast.LENGTH_LONG).show();
+    }
+
 }
